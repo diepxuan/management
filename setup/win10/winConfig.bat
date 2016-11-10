acddc740178db05fef58998b92e91f6373111026
Tran Ngoc Duc <caothu91@gmail.com>
2016-11-10 17:21:42 +0700

    win deny adobe

--- setup/win10/winConfig.bat
+++ setup/win10/winConfig.bat
@@ -11,10 +11,16 @@ rd "C:\OneDriveTemp" /Q /S
 REG Delete "HKEY_CLASSES_ROOT\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
 REG Delete "HKEY_CLASSES_ROOT\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" /f
 
-netsh advfirewall firewall add rule name="Deny CCleaner x64" dir=in action=allow program="C:\Program Files\CCleaner\CCleaner64.exe"
-netsh advfirewall firewall add rule name="Deny CCleaner" dir=in action=allow program="C:\Program Files\CCleaner\CCleaner.exe"
+rem netsh advfirewall firewall add rule name="Deny CCleaner x64" dir=in action=allow program="C:\Program Files\CCleaner\CCleaner64.exe"
+rem netsh advfirewall firewall add rule name="Deny CCleaner" dir=in action=allow program="C:\Program Files\CCleaner\CCleaner.exe"
 
-netsh advfirewall firewall add rule name="Deny CCleaner x64" dir=in action=block program="C:\Program Files\CCleaner\CCleaner64.exe"
-netsh advfirewall firewall add rule name="Deny CCleaner" dir=in action=block program="C:\Program Files\CCleaner\CCleaner.exe"
+rem netsh advfirewall firewall add rule name="Deny CCleaner x64" dir=in action=block program="C:\Program Files\CCleaner\CCleaner64.exe"
+rem netsh advfirewall firewall add rule name="Deny CCleaner" dir=in action=block program="C:\Program Files\CCleaner\CCleaner.exe"
+
+rem netsh advfirewall firewall add rule name="Deny Adobe" dir=in interface=any action=block remoteip=192.150.16.69
+rem netsh advfirewall firewall add rule name="Deny Adobe" dir=out interface=any action=block remoteip=192.150.16.69
+
+rem netsh advfirewall firewall add rule name="Deny Adobe" dir=in interface=any action=block remoteip=192.150.16.211
+rem netsh advfirewall firewall add rule name="Deny Adobe" dir=out interface=any action=block remoteip=192.150.16.211
 
 mklink /H D:\server\www\live\pma.diepxuan.vn\config.inc.php D:\server\www\code\php\phpmyadmin\config.inc.php
