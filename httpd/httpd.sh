e2ffd92bd08ab59915d9571bc3510eb89a90e87e
Tran Ngoc Duc <caothu91@gmail.com>
2017-01-14 11:16:14 +0700

    http - update vhost for diepxuan.com

--- httpd/httpd.sh
+++ httpd/httpd.sh
@@ -2,6 +2,7 @@
 cat httpd.conf > /etc/apache2/sites-available/ductn.conf
 
 a2ensite ductn.conf
-a2enmod proxy proxy_http headers deflate expires
+a2enmod proxy proxy_http headers deflate expires rewrite
 
 service apache2 restart
+service apache2 status
