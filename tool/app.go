054b6e467654e6bc7c1d783599968ee2857d0cf0
Tran Ngoc Duc <caothu91@gmail.com>
2017-02-06 23:14:42 +0700

    [tool] - run from window

--- tool/app.go
+++ tool/app.go
@@ -1,16 +1,10 @@
-// Copyright 2015 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
 package main
 
 import (
-	"github.com/caothu159/grunt"
 	"github.com/caothu159/window"
 	"github.com/google/gxui/drivers/gl"
 )
 
 func main() {
-	gl.StartDriver(window.appMain)
-	grunt.Grunt = grunt.CreateGrunt()
+	gl.StartDriver(window.Init)
 }
