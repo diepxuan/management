f3cac43e781b9c6e5060f2de6dc66e5d138de56b
Tran Ngoc Duc <caothu91@gmail.com>
2017-03-24 13:52:39 +0700

    http - update php

--- httpd/httpd.sh
+++ httpd/httpd.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 cat httpd.conf > /etc/apache2/sites-available/ductn.conf
 
+apt install php5.6* php7.0* php7.1*
+
 a2ensite ductn.conf
-a2enmod proxy proxy_http headers deflate expires rewrite vhost_alias
+a2dismod php5.5 php5.6 php7.1
+a2enmod proxy proxy_http headers deflate expires rewrite vhost_alias php7.0
 
 service apache2 restart
 service apache2 status
