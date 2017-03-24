8839b1c08f9e7ef48991a15fb2456a8d01d51a27
Tran Ngoc Duc <caothu91@gmail.com>
2017-03-24 11:10:07 +0700

    httpd - fix missing vhost_alias

--- httpd/httpd.sh
+++ httpd/httpd.sh
@@ -2,7 +2,7 @@
 cat httpd.conf > /etc/apache2/sites-available/ductn.conf
 
 a2ensite ductn.conf
-a2enmod proxy proxy_http headers deflate expires rewrite
+a2enmod proxy proxy_http headers deflate expires rewrite vhost_alias
 
 service apache2 restart
 service apache2 status
