1ed452a9afc8eee488ad39179bb47e9ac10d64ac
Tran Ngoc Duc <caothu91@gmail.com>
2017-08-02 19:38:01 +0700

    ssh | autorun

---
+++ ssh/ssh.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+cat config > ~/.ssh/config
+
+cat ductn > ~/.ssh/ductn
+cat ductn.pub > ~/.ssh/ductn.pub
+
+cat id_rsa > ~/.ssh/id_rsa
+cat id_rsa.pub > ~/.ssh/id_rsa.pub
+
+cat gss > ~/.ssh/gss
+cat gss.pub > ~/.ssh/gss.pub
