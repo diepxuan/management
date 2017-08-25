701a52f212773293cffca277bbadfa65b9470ed8
Tran Ngoc Duc <caothu91@gmail.com>
2017-08-25 10:08:23 +0700

    ssh | key_load_public: invalid format

--- setup/git/git.sh
+++ setup/git/git.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
-git config --global user.email "caothu91@gmail.com"
+# setting
 git config --global user.name "Tran Ngoc Duc"
-git config --global core.autocrlf input
+git config --global user.email "caothu91@gmail.com"
+
+# push
+git config --global push.default matching
+
+# file mode
+git config --global core.fileMode false
+
+# line endings
+git config --global core.autocrlf true
+git config --global core.eol lf
--- ssh/ssh.sh
+++ ssh/ssh.sh
@@ -4,6 +4,9 @@ cat config > ~/.ssh/config
 cat ductn > ~/.ssh/ductn
 cat ductn.pub > ~/.ssh/ductn.pub
 
+cat dx > ~/.ssh/dx
+cat dx.pub > ~/.ssh/dx.pub
+
 cat id_rsa > ~/.ssh/id_rsa
 cat id_rsa.pub > ~/.ssh/id_rsa.pub
 
