e3065c025be3cea25c2eb7bc97fcef6f25771a2f
Tran Ngoc Duc <caothu91@gmail.com>
2017-10-23 15:25:52 +0700

    git | fix broken config

--- setup/git/git.sh
+++ setup/git/git.sh
@@ -4,11 +4,11 @@ git config --global user.name "Tran Ngoc Duc"
 git config --global user.email "caothu91@gmail.com"
 
 # push
-git config --global push.default matching
+git config --global push.default simple
 
 # file mode
 git config --global core.fileMode false
 
 # line endings
-git config --global core.autocrlf true
+git config --global core.autocrlf false
 git config --global core.eol lf
