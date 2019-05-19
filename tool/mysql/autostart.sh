3ea6de39dd6f0033efa4a358c516cd368acc531b
Trần Ngọc Đức <caothu91@gmail.com>
2019-05-19 20:26:46 +0700

    0.1.5 fix swap off

--- tool/mysql/autostart.sh
+++ tool/mysql/autostart.sh
@@ -2,6 +2,11 @@
 
 if [[ ! "$(/usr/sbin/service mysql status)" =~ "start/running" ]]
 then
+    rm -rf /swapfile
+    fallocate -l 5G /swapfile
+    chmod 600 /swapfile
+    mkswap /swapfile
+    swapon /swapfile
     /usr/sbin/service mysql start
 fi
 
