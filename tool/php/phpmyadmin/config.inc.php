855bd142e2244b858811fdf7286dd2b48e065e85
Tran Ngoc Duc <caothu91@gmail.com>
2019-10-01 13:36:08 +0700

    create mysql replicate

--- tool/php/phpmyadmin/config.inc.php
+++ tool/php/phpmyadmin/config.inc.php
@@ -15,56 +15,65 @@
  * cookie. Needs to be 32 chars long.
  */
 $cfg['blowfish_secret']      = '51Lhp+H8YGJ2MgLaQZXpB$U$aIB+^f%20fVy'; /* YOU MUST FILL IN THIS FOR COOKIE AUTH! */
-$cfg['AllowArbitraryServer'] = false;
+$cfg['AllowArbitraryServer'] = true;
 
 /**
  * Servers configuration
  */
 $i = 0;
 
-/**
- * First server
- */
-$i ++;
-/* Authentication type */
-$cfg['Servers'][ $i ]['auth_type'] = 'cookie';
-
-/* Server parameters */
-$cfg['Servers'][ $i ]['host']            = 'localhost';
-$cfg['Servers'][ $i ]['compress']        = false;
-$cfg['Servers'][ $i ]['AllowNoPassword'] = false;
+$servers = [
+    'server.diepxuan.com',
+    'server2.diepxuan.com',
+    'server3.diepxuan.com',
+];
 
 /**
- * phpMyAdmin configuration storage settings.
+ * Initial server
  */
+foreach ( $servers as $server ) {
+    $i ++;
+    /* Authentication type */
+    $cfg['Servers'][ $i ]['auth_type'] = 'config';
+    $cfg['Servers'][ $i ]['user']      = 'sa';
+    $cfg['Servers'][ $i ]['password']  = 'bg2tob699';
+    /* Server parameters */
+    $cfg['Servers'][ $i ]['host']            = $server;
+    $cfg['Servers'][ $i ]['compress']        = false;
+    $cfg['Servers'][ $i ]['AllowNoPassword'] = false;
 
-/* User used to manipulate with storage */
-$cfg['Servers'][ $i ]['controlhost'] = 'localhost';
-$cfg['Servers'][ $i ]['controlport'] = '3306';
-$cfg['Servers'][ $i ]['controluser'] = 'sa';
-$cfg['Servers'][ $i ]['controlpass'] = 'bg2tob699';
+    /**
+     * phpMyAdmin configuration storage settings.
+     */
 
-/* Storage database and tables */
-$cfg['Servers'][ $i ]['pmadb']             = 'phpmyadmin';
-$cfg['Servers'][ $i ]['bookmarktable']     = 'pma__bookmark';
-$cfg['Servers'][ $i ]['relation']          = 'pma__relation';
-$cfg['Servers'][ $i ]['table_info']        = 'pma__table_info';
-$cfg['Servers'][ $i ]['table_coords']      = 'pma__table_coords';
-$cfg['Servers'][ $i ]['pdf_pages']         = 'pma__pdf_pages';
-$cfg['Servers'][ $i ]['column_info']       = 'pma__column_info';
-$cfg['Servers'][ $i ]['history']           = 'pma__history';
-$cfg['Servers'][ $i ]['table_uiprefs']     = 'pma__table_uiprefs';
-$cfg['Servers'][ $i ]['tracking']          = 'pma__tracking';
-$cfg['Servers'][ $i ]['userconfig']        = 'pma__userconfig';
-$cfg['Servers'][ $i ]['recent']            = 'pma__recent';
-$cfg['Servers'][ $i ]['favorite']          = 'pma__favorite';
-$cfg['Servers'][ $i ]['users']             = 'pma__users';
-$cfg['Servers'][ $i ]['usergroups']        = 'pma__usergroups';
-$cfg['Servers'][ $i ]['navigationhiding']  = 'pma__navigationhiding';
-$cfg['Servers'][ $i ]['savedsearches']     = 'pma__savedsearches';
-$cfg['Servers'][ $i ]['central_columns']   = 'pma__central_columns';
-$cfg['Servers'][ $i ]['designer_settings'] = 'pma__designer_settings';
-$cfg['Servers'][ $i ]['export_templates']  = 'pma__export_templates';
+    /* User used to manipulate with storage */
+    $cfg['Servers'][ $i ]['controlhost'] = 'localhost';
+    $cfg['Servers'][ $i ]['controlport'] = '3306';
+    $cfg['Servers'][ $i ]['controluser'] = 'sa';
+    $cfg['Servers'][ $i ]['controlpass'] = 'bg2tob699';
+
+    /* Storage database and tables */
+    $cfg['Servers'][ $i ]['pmadb']             = 'phpmyadmin';
+    $cfg['Servers'][ $i ]['bookmarktable']     = 'pma__bookmark';
+    $cfg['Servers'][ $i ]['relation']          = 'pma__relation';
+    $cfg['Servers'][ $i ]['table_info']        = 'pma__table_info';
+    $cfg['Servers'][ $i ]['table_coords']      = 'pma__table_coords';
+    $cfg['Servers'][ $i ]['pdf_pages']         = 'pma__pdf_pages';
+    $cfg['Servers'][ $i ]['column_info']       = 'pma__column_info';
+    $cfg['Servers'][ $i ]['history']           = 'pma__history';
+    $cfg['Servers'][ $i ]['table_uiprefs']     = 'pma__table_uiprefs';
+    $cfg['Servers'][ $i ]['tracking']          = 'pma__tracking';
+    $cfg['Servers'][ $i ]['userconfig']        = 'pma__userconfig';
+    $cfg['Servers'][ $i ]['recent']            = 'pma__recent';
+    $cfg['Servers'][ $i ]['favorite']          = 'pma__favorite';
+    $cfg['Servers'][ $i ]['users']             = 'pma__users';
+    $cfg['Servers'][ $i ]['usergroups']        = 'pma__usergroups';
+    $cfg['Servers'][ $i ]['navigationhiding']  = 'pma__navigationhiding';
+    $cfg['Servers'][ $i ]['savedsearches']     = 'pma__savedsearches';
+    $cfg['Servers'][ $i ]['central_columns']   = 'pma__central_columns';
+    $cfg['Servers'][ $i ]['designer_settings'] = 'pma__designer_settings';
+    $cfg['Servers'][ $i ]['export_templates']  = 'pma__export_templates';
+}
 
 /**
  * End of servers configuration
@@ -96,7 +105,7 @@ $cfg['SaveDir']   = '';
  * Possible values: 25, 50, 100, 250, 500
  * default = 25
  */
-$cfg['MaxRows'] = 250;
+//$cfg['MaxRows'] = 50;
 
 /**
  * Disallow editing of binary fields
