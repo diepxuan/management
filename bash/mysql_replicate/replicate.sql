92dc00df0825c9010a83fa40f04cdc4ed349b913
Tran Ngoc Duc <caothu91@gmail.com>
2020-11-19 14:52:56 +0700

    update mysql galera

--- bash/mysql_replicate/replicate.sql
+++ bash/mysql_replicate/replicate.sql
@@ -1,9 +1,15 @@
-SET GLOBAL validate_password.policy=LOW;
+UNINSTALL COMPONENT "file://component_validate_password";
+UNINSTALL PLUGIN validate_password;
+
 CREATE USER IF NOT EXISTS 'slave' IDENTIFIED WITH mysql_native_password BY 'ductn@123';
 CREATE USER IF NOT EXISTS 'sa' IDENTIFIED WITH mysql_native_password BY 'ductn@123';
 GRANT REPLICATION SLAVE ON *.* TO 'slave';
 GRANT ALL PRIVILEGES ON * . * TO 'sa';
+
+-- INSTALL COMPONENT "file://component_validate_password";
+INSTALL PLUGIN validate_password SONAME 'validate_password.so';
 -- SET GLOBAL validate_password.policy=MEDIUM;
+SET GLOBAL validate_password.policy=LOW;
 FLUSH PRIVILEGES;
 
 STOP SLAVE;
