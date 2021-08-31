bcfd9422d59b4e4d2cf3c98cb71c19a92eba8b86
Tran Ngoc Duc <caothu91@gmail.com>
2021-08-31 16:20:15 +0700

    update ductn bash

--- bash/completion/ductn.sh
+++ bash/completion/ductn.sh
@@ -23,9 +23,9 @@ _ductn() {
 
         case "$com" in
 
-        # apt:fix)
-        #     opts="${opts} --service --update"
-        #     ;;
+        apt:fix)
+            opts="${opts}"
+            ;;
 
         cron)
             opts="${opts} --service --update"
@@ -43,6 +43,10 @@ _ductn() {
             opts="${opts} --install"
             ;;
 
+        ssh:install)
+            opts="${opts} --install"
+            ;;
+
         httpd:install)
             opts="${opts}"
             ;;
@@ -55,124 +59,20 @@ _ductn() {
             opts="${opts}"
             ;;
 
-        setup:cron:run)
-            opts="${opts} --magento-init-params"
+        ddns:update)
+            opts="${opts}"
             ;;
 
-        setup:db-data:upgrade)
-            opts="${opts} --magento-init-params"
+        cron:update)
+            opts="${opts}"
             ;;
 
-        setup:db-schema:upgrade)
-            opts="${opts} --magento-init-params"
+        cron:service)
+            opts="${opts}"
             ;;
 
-        setup:db:status)
-            opts="${opts} --magento-init-params"
-            ;;
-
-        setup:di:compile)
-            opts="${opts} "
-            ;;
-
-        setup:install)
-            opts="${opts} --backend-frontname --key --db-host --db-name --db-user --db-engine --db-password --db-prefix --db-model --db-init-statements --skip-db-validation --http-cache-hosts --session-save --session-save-redis-host --session-save-redis-port --session-save-redis-password --session-save-redis-timeout --session-save-redis-persistent-id --session-save-redis-db --session-save-redis-compression-threshold --session-save-redis-compression-lib --session-save-redis-log-level --session-save-redis-max-concurrency --session-save-redis-break-after-frontend --session-save-redis-break-after-adminhtml --session-save-redis-first-lifetime --session-save-redis-bot-first-lifetime --session-save-redis-bot-lifetime --session-save-redis-disable-locking --session-save-redis-min-lifetime --session-save-redis-max-lifetime --cache-backend --cache-backend-redis-server --cache-backend-redis-db --cache-backend-redis-port --page-cache --page-cache-redis-server --page-cache-redis-db --page-cache-redis-port --page-cache-redis-compress-data --base-url --language --timezone --currency --use-rewrites --use-secure --base-url-secure --use-secure-admin --admin-use-security-key --admin-user --admin-password --admin-email --admin-firstname --admin-lastname --cleanup-database --sales-order-increment-prefix --use-sample-data --interactive --magento-init-params"
-            ;;
-
-        setup:performance:generate-fixtures)
-            opts="${opts} --skip-reindex"
-            ;;
-
-        setup:rollback)
-            opts="${opts} --code-file --media-file --db-file --magento-init-params"
-            ;;
-
-        setup:static-content:deploy)
-            opts="${opts} --force --strategy --area --exclude-area --theme --exclude-theme --language --exclude-language --jobs --symlink-locale --content-version --refresh-content-version-only --no-javascript --no-css --no-less --no-images --no-fonts --no-html --no-misc --no-html-minify"
-            ;;
-
-        setup:store-config:set)
-            opts="${opts} --base-url --language --timezone --currency --use-rewrites --use-secure --base-url-secure --use-secure-admin --admin-use-security-key --magento-init-params"
-            ;;
-
-        setup:uninstall)
-            opts="${opts} --magento-init-params"
-            ;;
-
-        setup:upgrade)
-            opts="${opts} --keep-generated --magento-init-params"
-            ;;
-
-        store:list)
-            opts="${opts} "
-            ;;
-
-        store:website:list)
-            opts="${opts} "
-            ;;
-
-        sys:init)
-            opts="${opts} --server --admin"
-            ;;
-
-        sys:init:install)
-            opts="${opts} --server --admin"
-            ;;
-
-        sys:cron:install)
-            opts="${opts} --timezone --format"
-            ;;
-
-        sys:cron:list)
-            opts="${opts} --format"
-            ;;
-
-        sys:cron:run)
-            opts="${opts} "
-            ;;
-
-        sys:cron:schedule)
-            opts="${opts} "
-            ;;
-
-        sys:info)
-            opts="${opts} --format"
-            ;;
-
-        sys:maintenance)
-            opts="${opts} --on --off"
-            ;;
-
-        sys:setup:change-version)
-            opts="${opts} "
-            ;;
-
-        sys:setup:compare-versions)
-            opts="${opts} --ignore-data --log-junit --format"
-            ;;
-
-        sys:setup:downgrade-versions)
-            opts="${opts} --dry-run"
-            ;;
-
-        sys:store:config:base-url:list)
-            opts="${opts} --format"
-            ;;
-
-        sys:store:list)
-            opts="${opts} --format"
-            ;;
-
-        sys:url:list)
-            opts="${opts} --add-categories --add-products --add-cmspages --add-all"
-            ;;
-
-        sys:website:list)
-            opts="${opts} --format"
-            ;;
-
-        theme:uninstall)
-            opts="${opts} --backup-code --clear-static-content"
+        sys:ddns-allow)
+            opts="${opts}"
             ;;
 
         user:new)
@@ -180,6 +80,14 @@ _ductn() {
             ;;
 
         self-update)
+            opts="${opts} "
+            ;;
+
+        log:watch)
+            opts="${opts} "
+            ;;
+
+        log:cleanup)
             opts="${opts} "
             ;;
 
@@ -193,7 +101,15 @@ _ductn() {
 
     # completing for a command
     if [[ $cur == $com ]]; then
-        coms="apt:fix git ssl ssh sys:init self-update user:new httpd:install httpd:config mysql:ssl:enable"
+        coms=("sqlsrv:php:install" "mssql:php:install" "sqlsrv:php:enable" "mssql:php:enable")
+
+        coms+=("apt:fix" "git" "ssl" "ssh" "ssh:install" "sys:init" "self-update" "user:new" "httpd:install")
+        coms+=("httpd:config" "mysql:ssl:enable" "cron:update" "cron:service" "ddns:update" "sys:ddns-allow")
+        coms+=("log:watch" "log:cheanup")
+
+        separator=" "
+        coms="$(printf "${separator}%s" "${coms[@]}")"
+        coms="${coms:${#separator}}"
 
         COMPREPLY=($(compgen -W "${coms}" -- ${cur}))
         __ltrim_colon_completions "$cur"
