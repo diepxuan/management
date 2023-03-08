c779046f7d331c957c638a32516550631aba3550
Tran Ngoc Duc <caothu91@gmail.com>
2023-03-08 14:16:32 +0700

    iptables | fix save to service

--- bash/bin/ufw.rules.sh
+++ bash/bin/ufw.rules.sh
@@ -12,13 +12,15 @@ _DUCTN_COMMANDS+=("ufw:iptables")
     _SRV_NUM=${_SRV_NUM:3}
 
     function _rule_out() {
-        _rule=$@
-        # [[ -n $_rule ]] && echo -e "ExecStart=$iptables_path\t$_rule" | sudo tee -a /usr/lib/systemd/system/ductn-iptables.service
-        [[ -n $_rule ]] && iptables_rules_HEAD="$iptables_rules_HEAD\nExecStart=$iptables_path $_rule"
-        _rule=${_rule//'-A '/'-D '}
-        _rule=${_rule//'-I '/'-D '}
-        # [[ -n $_rule ]] && echo -e "ExecStop=$iptables_path\t$_rule" | sudo tee -a /usr/lib/systemd/system/ductn-iptables.service
-        [[ -n $_rule ]] && iptables_rules_FOOT="$iptables_rules_FOOT\nExecStop=$iptables_path $_rule"
+        # echo -e "$@"
+        while IFS= read -r _rule; do
+            # [[ -n $_rule ]] && echo -e "ExecStart=$iptables_path\t$_rule" | sudo tee -a /usr/lib/systemd/system/ductn-iptables.service
+            [[ -n $_rule ]] && iptables_rules_HEAD="$iptables_rules_HEAD\nExecStart=$iptables_path $_rule"
+            _rule=${_rule//'-A '/'-D '}
+            _rule=${_rule//'-I '/'-D '}
+            # [[ -n $_rule ]] && echo -e "ExecStop=$iptables_path\t$_rule" | sudo tee -a /usr/lib/systemd/system/ductn-iptables.service
+            [[ -n $_rule ]] && iptables_rules_FOOT="$iptables_rules_FOOT\nExecStop=$iptables_path $_rule"
+        done <<<"$@"
     }
 
     function _rule_nat() {
@@ -53,27 +55,46 @@ _DUCTN_COMMANDS+=("ufw:iptables")
         _rule_out "-t raw -I PREROUTING -i fwbr+ -j CT --zone 1"
         [[ "$(ip tuntap show | grep tun0)" != "" ]] && _rule_out "-t nat -A POSTROUTING -s '10.0.$_SRV_NUM.0/24' -o tun0 -j MASQUERADE"
         # NAT port to vm client
-        _rule_nat "10.0.$_SRV_NUM.10" 3389
-        _rule_nat "10.0.$_SRV_NUM.11" 1433
+        _rule_out "-t nat -A PREROUTING -p TCP --dport 3389 -j DNAT --to-destination 10.0.$_SRV_NUM.10"
+        _rule_out "-t nat -A PREROUTING -p TCP --dport 1433 -j DNAT --to-destination 10.0.$_SRV_NUM.11"
     fi
 
     ######### VPN Firewall DMZ to Pve server #########
     if [[ $(--host:is_vpn_server) == 1 ]]; then
-        _rule_nat "10.8.0.2" 3389
-        _rule_nat "10.8.0.2" 1433
+        # load modules
+        sudo /sbin/depmod -a
+        sudo /sbin/modprobe ip_tables
+        sudo /sbin/modprobe ip_conntrack
+        sudo /sbin/modprobe iptable_filter
+        sudo /sbin/modprobe iptable_mangle
+        sudo /sbin/modprobe iptable_nat
+        sudo /sbin/modprobe ipt_LOG
+        sudo /sbin/modprobe ipt_limit
+        sudo /sbin/modprobe ipt_state
+        sudo /sbin/modprobe ipt_MASQUERADE
 
-        # Do some checks for obviously spoofed IP's
-        _INET_IP="$(--ip:wan)"
-        _INET_IF="$(route | grep '^default' | grep -o '[^ ]*$')"
-        _rule_out "-t nat -A PREROUTING -i $_INET_IF -s $_INET_IP -j DROP"
+        # sudo $iptables_path -N bad_tcp_packets
+        # sudo $iptables_path -N allowed
+        # sudo $iptables_path -N icmp_packets
 
-        # Get rid of bad TCP packets
-        _rule_out "-A FORWARD -p tcp ! --syn -m state --state NEW -j LOG --log-prefix \"New not syn: \""
-        _rule_out "-A FORWARD -p tcp ! --syn -m state --state NEW -j DROP"
+        _rule_out "$(--ufw:dmz)"
+        # _rule_out "-A FORWARD -p tcp ! --syn -m state --state NEW -j DROP"
 
-        # General rules
-        _rule_out "-A FORWARD -i tun0 -o $_INET_IF -j ACCEPT"
-        _rule_out "-A FORWARD -i $_INET_IF -o tun0 -m state --state ESTABLISHED,RELATED -j ACCEPT"
+        # _rule_nat "10.8.0.2" 3389
+        # _rule_nat "10.8.0.2" 1433
+
+        # # Do some checks for obviously spoofed IP's
+        # _INET_IP="$(--ip:wan)"
+        # _INET_IF="$(route | grep '^default' | grep -o '[^ ]*$')"
+        # _rule_out "-t nat -A PREROUTING -i $_INET_IF -s $_INET_IP -j DROP"
+
+        # # Get rid of bad TCP packets
+        # _rule_out "-A FORWARD -p tcp ! --syn -m state --state NEW -j LOG --log-prefix \"New not syn: \""
+        # _rule_out "-A FORWARD -p tcp ! --syn -m state --state NEW -j DROP"
+
+        # # General rules
+        # _rule_out "-A FORWARD -i tun0 -o $_INET_IF -j ACCEPT"
+        # _rule_out "-A FORWARD -i $_INET_IF -o tun0 -m state --state ESTABLISHED,RELATED -j ACCEPT"
 
         # "-t nat -A PREROUTING -i $eth0 -p tcp -d ${PUBLIC_IP} --dport 80 -j DNAT --to ${INTERNAL_IP}:80"
         # "-t nat -A PREROUTING -p TCP -i $_INET_IF -d $_INET_IP --dport 80 -j DNAT --to-destination 10.8.0.2"
@@ -106,6 +127,10 @@ WantedBy=multi-user.target" | sudo tee /usr/lib/systemd/system/ductn-iptables.se
     sudo systemctl daemon-reload
     sudo systemctl enable ductn-iptables
     sudo systemctl restart ductn-iptables
+
+    if [ "$(--sys:service:isactive ductn-iptables)" == "failed" ]; then
+        sudo systemctl status ductn-iptables
+    fi
 }
 
 --ufw:iptables:path4() {
@@ -127,7 +152,67 @@ WantedBy=multi-user.target" | sudo tee /usr/lib/systemd/system/ductn-iptables.se
     echo $ip6tables_path
 }
 
---ufw:DMX() {
+--ufw:dmz() {
+    DMZ_RULES=""
+
+    INET_IP="$(--ip:wan)"
+    INET_IFACE="$(route | grep '^default' | grep -o '[^ ]*$')"
+
+    LAN_IP="$(--ip:local)"
+    # LAN_IFACE="tun0"
+
+    DMZ_IP="10.8.0.2"
+    DMZ_IFACE="tun0"
+
+    LO_IFACE="lo"
+    LO_IP="127.0.0.1"
+
+    echo "-A bad_tcp_packets -p tcp ! --syn -m state --state NEW -j LOG --log-prefix \"New not syn:\""
+    echo "-A bad_tcp_packets -p tcp ! --syn -m state --state NEW -j DROP"
+
+    echo "-A allowed -p TCP --syn -j ACCEPT"
+    echo "-A allowed -p TCP -m state --state ESTABLISHED,RELATED -j ACCEPT"
+    echo "-A allowed -p TCP -j DROP"
+
+    echo "-A icmp_packets -p ICMP -s 0/0 --icmp-type 0 -j ACCEPT"
+    echo "-A icmp_packets -p ICMP -s 0/0 --icmp-type 3 -j ACCEPT"
+    echo "-A icmp_packets -p ICMP -s 0/0 --icmp-type 5 -j ACCEPT"
+    echo "-A icmp_packets -p ICMP -s 0/0 --icmp-type 8 -j ACCEPT"
+    echo "-A icmp_packets -p ICMP -s 0/0 --icmp-type 11 -j ACCEPT"
+
+    echo "-A INPUT -p tcp -j bad_tcp_packets"
+    echo "-A INPUT -p ICMP -i $INET_IFACE -j icmp_packets"
+    echo "-A INPUT -p ALL -i $DMZ_IFACE -d $DMZ_IP -j ACCEPT"
+    # echo "-A INPUT -p ALL -i $LAN_IFACE -d $LAN_IP -j ACCEPT"
+    # echo "-A INPUT -p ALL -i $LAN_IFACE -d $LAN_BROADCAST_ADDRESS -j ACCEPT"
+    echo "-A INPUT -p ALL -i $LO_IFACE -s $LO_IP -j ACCEPT"
+    echo "-A INPUT -p ALL -i $LO_IFACE -s $LAN_IP -j ACCEPT"
+    echo "-A INPUT -p ALL -i $LO_IFACE -s $INET_IP -j ACCEPT"
+    echo "-A INPUT -p ALL -d $INET_IP -m state --state ESTABLISHED,RELATED -j ACCEPT"
+    echo "-A INPUT -m limit --limit 3/minute --limit-burst 3 -j LOG --log-prefix \"IPT INPUT packet died: \""
+
+    echo "-A FORWARD -p tcp -j bad_tcp_packets"
+    echo "-A FORWARD -i $DMZ_IFACE -o $INET_IFACE -j ACCEPT"
+    echo "-A FORWARD -i $INET_IFACE -o $DMZ_IFACE -m state --state ESTABLISHED,RELATED -j ACCEPT"
+    # echo "-A FORWARD -i $LAN_IFACE -o $DMZ_IFACE -j ACCEPT"
+    # echo "-A FORWARD -i $DMZ_IFACE -o $LAN_IFACE -m state --state ESTABLISHED,RELATED -j ACCEPT"
+    echo "-A FORWARD -m limit --limit 3/minute --limit-burst 3 -j LOG --log-prefix \"IPT FORWARD packet died: \""
+
+    echo "-A OUTPUT -p tcp -j bad_tcp_packets"
+    echo "-A OUTPUT -m limit --limit 3/minute --limit-burst 3 -j LOG --log-prefix \"IPT OUTPUT packet died: \""
+
+    echo "-t nat -A POSTROUTING -o $INET_IFACE -j SNAT --to-source $INET_IP"
+    echo "-t nat -A PREROUTING -p TCP -i $INET_IFACE -d $INET_IP --dport 3389 -j DNAT --to-destination $DMZ_IP"
+    echo "-t nat -A PREROUTING -p TCP -i $INET_IFACE -d $INET_IP --dport 53 -j DNAT --to-destination $DMZ_IP"
+    echo "-t nat -A PREROUTING -p UDP -i $INET_IFACE -d $INET_IP --dport 53 -j DNAT --to-destination $DMZ_IP"
+
+    echo "-t nat -A INPUT -i $INET_IFACE -s 192.168.0.0/16 -j DROP"
+    echo "-t nat -A INPUT -i $INET_IFACE -s 10.0.0.0/8 -j DROP"
+    echo "-t nat -A INPUT -i $INET_IFACE -s 172.16.0.0/12 -j DROP"
+    echo "-t nat -A INPUT -i $INET_IFACE -s $INET_IP -j DROP"
+}
+
+--ufw:DMZ_example() {
     #!/bin/sh
     #
     # rc.DMZ.firewall - DMZ IP Firewall script for Linux 2.4.x and iptables
@@ -568,4 +653,164 @@ WantedBy=multi-user.target" | sudo tee /usr/lib/systemd/system/ductn-iptables.se
     #
     # 4.3.8 POSTROUTING chain
     #
+}
+
+--ufw:DMZ_example2() {
+    #!/bin/sh
+    #
+    # rc.DMZ.firewall - DMZ IP Firewall script for Linux 2.4.x
+    #
+    # Author: Oskar Andreasson <blueflux@koffein.net>
+    # (c) of BoingWorld.com, use at your own risk, do whatever you please with
+    # it as long as you don't distribute this without due credits to
+    # BoingWorld.com
+    #
+
+    ###########
+    # Configuration options, these will speed you up getting this script to
+    # work with your own setup.
+
+    #
+    # your LAN's IP range and localhost IP. /24 means to only use the first 24
+    # bits of the 32 bit IP adress. the same as netmask 255.255.255.0
+    #
+    # STATIC_IP is used by me to allow myself to do anything to myself, might
+    # be a security risc but sometimes I want this. If you don't have a static
+    # IP, I suggest not using this option at all for now but it's stil
+    # enabled per default and will add some really nifty security bugs for all
+    # those who skips reading the documentation=)
+
+    LAN_IP="192.168.0.2"
+    LAN_BCAST_ADRESS="192.168.0.255"
+    LAN_IFACE="eth1"
+
+    INET_IP="194.236.50.152"
+    INET_IFACE="eth0"
+
+    HTTP_IP="194.236.50.153"
+    DNS_IP="194.236.50.154"
+    DMZ_HTTP_IP="192.168.1.2"
+    DMZ_DNS_IP="192.168.1.3"
+    DMZ_IP="192.168.1.1"
+    DMZ_IFACE="eth2"
+
+    LO_IP="127.0.0.1"
+    LO_IFACE="lo"
+
+    IPTABLES="/usr/local/sbin/iptables"
+
+    ###########################################
+    #
+    # Load all required IPTables modules unless compiled into the kernel
+    #
+
+    #
+    # Needed to initially load modules
+    #
+
+    /sbin/depmod -a
+
+    #
+    # Adds some iptables targets like LOG, REJECT and MASQUARADE.
+    #
+
+    /sbin/modprobe ipt_LOG
+    /sbin/modprobe ipt_MASQUERADE
+
+    #
+    # Support for connection tracking of FTP and IRC.
+    #
+    #/sbin/modprobe ip_conntrack_ftp
+    #/sbin/modprobe ip_conntrack_irc
+
+    #CRITICAL:  Enable IP forwarding since it is disabled by default.
+    #
+
+    echo "1" >/proc/sys/net/ipv4/ip_forward
+
+    #
+    # Dynamic IP users:
+    #
+    #echo "1" > /proc/sys/net/ipv4/ip_dynaddr
+
+    ###########################################
+    #
+    # Chain Policies gets set up before any bad packets gets through
+    #
+
+    $IPTABLES -P INPUT DROP
+    $IPTABLES -P OUTPUT DROP
+    $IPTABLES -P FORWARD DROP
+
+    #
+    # the allowed chain for TCP connections, utilized in the FORWARD chain
+    #
+
+    $IPTABLES -N allowed
+    $IPTABLES -A allowed -p TCP --syn -j ACCEPT
+    $IPTABLES -A allowed -p TCP -m state --state ESTABLISHED,RELATED -j ACCEPT
+    $IPTABLES -A allowed -p TCP -j DROP
+
+    #
+    # ICMP rules, utilized in the FORWARD chain
+    #
+
+    $IPTABLES -N icmp_packets
+    $IPTABLES -A icmp_packets -p ICMP -s 0/0 --icmp-type 0 -j ACCEPT
+    $IPTABLES -A icmp_packets -p ICMP -s 0/0 --icmp-type 3 -j ACCEPT
+    $IPTABLES -A icmp_packets -p ICMP -s 0/0 --icmp-type 5 -j ACCEPT
+    $IPTABLES -A icmp_packets -p ICMP -s 0/0 --icmp-type 11 -j ACCEPT
+
+    ###########################################
+    # POSTROUTING chain in the nat table
+    #
+    # Enable IP SNAT for all internal networks trying to get out on the Internet
+    #
+
+    $IPTABLES -t nat -A POSTROUTING -o $INET_IFACE -j SNAT --to-source $INET_IP
+
+    ###########################################
+    # PREROUTING chain in the nat table
+    #
+    # Do some checks for obviously spoofed IP's
+    #
+
+    $IPTABLES -t nat -A PREROUTING -i $INET_IFACE -s 192.168.0.0/16 -j DROP
+    $IPTABLES -t nat -A PREROUTING -i $INET_IFACE -s 10.0.0.0/8 -j DROP
+    $IPTABLES -t nat -A PREROUTING -i $INET_IFACE -s 172.16.0.0/12 -j DROP
+    $IPTABLES -t nat -A PREROUTING -i $INET_IFACE -s $INET_IP -j DROP
+
+    #
+    # Enable IP Destination NAT for DMZ zone
+    #
+
+    $IPTABLES -t nat -A PREROUTING -p TCP -i $INET_IFACE -d $HTTP_IP --dport 80 \
+        -j DNAT --to-destination $DMZ_HTTP_IP
+    $IPTABLES -t nat -A PREROUTING -p TCP -i $INET_IFACE -d $DNS_IP --dport 53 \
+        -j DNAT --to-destination $DMZ_DNS_IP
+    $IPTABLES -t nat -A PREROUTING -p UDP -i $INET_IFACE -d $DNS_IP --dport 53 \
+        -j DNAT --to-destination $DMZ_DNS_IP
+
+    ###########################################
+    #
+    # FORWARD chain
+    #
+    # Get rid of bad TCP packets
+    #
+
+    $IPTABLES -A FORWARD -p tcp ! --syn -m state --state NEW -j LOG \
+        --log-prefix "New not syn:"
+    $IPTABLES -A FORWARD -p tcp ! --syn -m state --state NEW -j DROP
+
+    #
+    # DMZ section
+    #
+    # General rules
+    #
+
+    $IPTABLES -A FORWARD -i $DMZ_IFACE -o $INET_IFACE -j ACCEPT
+    $IPTABLES -A FORWARD -i $INET_IFACE -o $DMZ_IFACE -m state \
+        --state ESTABLISHED,RELATED -j ACCEPT
+    $IPTABLES -A FORWARD -i $LAN_IFACE -o $DMZ_IFAC
+
 }
