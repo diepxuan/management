0810b04467620db89e905b244876a0bc138311d8
Tran Ngoc Duc <caothu91@gmail.com>
2023-03-19 19:53:02 +0700

    fix csf rules missing in vpn server

--- var/lib/dev.sh
+++ var/lib/dev.sh
@@ -3,6 +3,9 @@
 
 _DUCTN_COMMANDS+=("dev:build")
 --dev:build() {
+    # changelog=$(cat debian/changelog)
+    cat debian/changelog | sed -e "s/<ductn@diepxuan.com>  .*/<ductn@diepxuan.com>  $(date -R)/g" >debian/changelog
+
     dpkg-buildpackage
 }
 
