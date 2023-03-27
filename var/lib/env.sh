b75622596fe82a23c114d788698f79968048b864
Tran Ngoc Duc <caothu91@gmail.com>
2023-03-27 14:43:18 +0700

    * FUTURE: add dhcp to online config

--- var/lib/env.sh
+++ var/lib/env.sh
@@ -44,6 +44,36 @@
 
 }
 
+--sys:env:dhcp() {
+    dhcp_name=$1
+    dhcp_type="vm_$2"
+    dhcp_types=("vm_mac" "vm_address")
+
+    while IFS= read -r line; do
+        [[ "$line" =~ ^#.*$ ]] && continue
+        [[ -z "$line" ]] && continue
+        line=$(echo $line | xargs)
+        readarray -d ' ' -t strarr <<<"$line"
+        # declare -p strarr
+
+        vm_name=${strarr[0]}
+        vm_mac=${strarr[1]}
+        vm_address=${strarr[2]}
+
+        if [[ -z $dhcp_name ]]; then
+            echo $vm_name | xargs
+        elif [[ $dhcp_name == $vm_name ]]; then
+            if [[ " ${dhcp_types[*]} " =~ " ${dhcp_type} " ]]; then
+                echo -e "${!dhcp_type}" | xargs
+            fi
+        fi
+
+        unset vm_name vm_mac vm_address
+    done <$ETC_PATH/dhcp
+
+    unset dhcp_name dhcp_type dhcp_types
+}
+
 --sys:env:vpn() {
     cat $ETC_PATH/tunel
 }
@@ -64,9 +94,13 @@
                 --csf:regex
                 [[ ! $_old == $_new ]] && --csf:config
             fi
+
+            if [[ $param == "dhcp" ]]; then
+                [[ ! $_old == $_new ]] && --sys:dhcp:config
+            fi
             unset _new _old
         done
     }
 
-    _sync domains portforward tunel csf
+    _sync domains portforward tunel csf dhcp
 }
