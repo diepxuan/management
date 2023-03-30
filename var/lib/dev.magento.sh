20b80fe42b8adbe8b85d39d7825c7270db014348
Tran Ngoc Duc <caothu91@gmail.com>
2023-03-30 14:23:13 +0700

    * FIXBUG: dev libraries

--- var/lib/dev.magento.sh
+++ var/lib/dev.magento.sh
@@ -4,7 +4,7 @@
 WEBSERVER_GROUP="www-data"
 
 _DUCTN_M2+=(ch)
-_dev:ch() {
+_dev:m2:ch() {
     _ch() {
         chmod -R g+w \${1}
         chmod g+ws \${1}
@@ -36,77 +36,77 @@ _dev:ch() {
 }
 
 _DUCTN_M2+=(group)
-_dev:group() {
+_dev:m2:group() {
     usermod -aG $WEBSERVER_GROUP $(whoami)
 }
 
 _DUCTN_M2+=(urn)
-_dev:urn() {
+_dev:m2:urn() {
     bin/magento dev:urn-catalog:generate .idea/misc.xml
 }
 
 _DUCTN_M2+=(perm)
-_dev:perm() {
+_dev:m2:perm() {
     chown -R $(whoami):$WEBSERVER_GROUP .
     chmod u+x bin/magento
-    _dev:ch
+    _dev:m2:ch
 }
 
 _DUCTN_M2+=(rmgen)
-_dev:rmgen() {
+_dev:m2:rmgen() {
     find generated generated/code generation generation/code var/generation -maxdepth 1 -mindepth 1 -type d -not -name 'Magento' -not -name 'Composer' -not -name 'Symfony' -print0 -printf '\r\n' -exec rm -rf {} \;
     magerun2 generation:flush
 }
 
 _DUCTN_M2+=(static)
-_dev:static() {
+_dev:m2:static() {
     rm -rf var/view_preprocessed/* pub/static/frontend/* pub/static/adminhtml/* pub/static/_requirejs/*
     magerun2 dev:asset:clear
 }
 
 _DUCTN_M2+=(cache)
-_dev:cache() {
+_dev:m2:cache() {
     rm -rf var/cache/* var/page_cache/* var/tmp/* var/generation/* var/di/*
     magerun2 cache:clean
     magerun2 cache:flush
 }
 
 _DUCTN_M2+=(index)
-_dev:index() {
+_dev:m2:index() {
     magerun2 indexer:reindex
 }
 
 _DUCTN_M2+=(grunt)
-_dev:grunt() {
-    _dev:rmgen
-    _dev:static
-    _dev:cache
+_dev:m2:grunt() {
+    _dev:m2:rmgen
+    _dev:m2:static
+    _dev:m2:cache
     # bin/magento setup:upgrade
     grunt exec:all
-    _dev:perm
+    _dev:m2:perm
     grunt watch
 }
 
 _DUCTN_M2+=(up)
-_dev:up() {
-    _dev:perm
-    _dev:rmgen
-    _dev:static
-    _dev:cache
+_dev:m2:up() {
+    _dev:m2:perm
+    _dev:m2:rmgen
+    _dev:m2:static
+    _dev:m2:cache
     bin/magento setup:upgrade
-    _dev:perm
+    _dev:m2:perm
 }
 
 _DUCTN_M2+=(config)
-_dev:config() {
-    _dev:perm
+_dev:m2:config() {
+    _dev:m2:perm
     magerun2 module:enable --all
     magerun2 setup:di:compile
-    _dev:ch
+    _dev:m2:ch
 }
 
 _DUCTN_M2+=(setting)
-_dev:setting() {
+_dev:m2:setting() {
     magerun2 config:store:set admin/security/admin_account_sharing 1
     magerun2 config:store:set admin/security/use_form_key 0
     magerun2 config:store:set admin/startup/page dashboard
@@ -140,17 +140,17 @@ _dev:setting() {
 
     magerun2 admin:notifications --off
 
-    _dev:cache
+    _dev:m2:cache
 
 }
 
 _DUCTN_M2+=(developer)
-_dev:developer() {
+_dev:m2:developer() {
     composer -vvv require --dev diepxuan/module-email
-    _dev:perm
-    _dev:rmgen
-    _dev:static
-    _dev:cache
+    _dev:m2:perm
+    _dev:m2:rmgen
+    _dev:m2:static
+    _dev:m2:cache
 
     magerun2 maintenance:enable
     magerun2 deploy:mode:set developer
@@ -159,17 +159,17 @@ _dev:developer() {
     magerun2 setup:di:compile
     magerun2 maintenance:disable
 
-    _dev:cache
-    _dev:perm
+    _dev:m2:cache
+    _dev:m2:perm
 }
 
 _DUCTN_M2+=(delsolr)
-_dev:delsolr() {
+_dev:m2:delsolr() {
     sudo su solr -c \"/opt/solr/bin/solr delete -c \${1}\"
 }
 
 _DUCTN_M2+=(addsolr)
-_dev:addsolr() {
+_dev:m2:addsolr() {
     sudo su solr -c \"/opt/solr/bin/solr delete -c \${1}\"
     sudo su solr -c \"/opt/solr/bin/solr create -c \${1}\"
     cat vendor/partsbx/core/src/module-partsbx-solr/conf/managed-schema | sudo su solr -c \"tee /var/solr/data/\${1}\/conf/managed-schema\"
@@ -178,29 +178,34 @@ _dev:addsolr() {
 }
 
 _DUCTN_M2+=(logenable)
-_dev:logenable() {
+_dev:m2:logenable() {
     bin/magento dev:query-log:enable
 }
 
 _DUCTN_M2+=(logdisable)
-_dev:logdisable() {
+_dev:m2:logdisable() {
     bin/magento dev:query-log:disable
 }
 
 _DUCTN_M2+=(tempdebugenable)
-_dev:tempdebugenable() {
+_dev:m2:tempdebugenable() {
     magerun2 dev:template-hints-blocks --on
     magerun2 dev:template-hints --on
 }
 
 _DUCTN_M2+=(tempdebugdisable)
-_dev:tempdebugdisable() {
+_dev:m2:tempdebugdisable() {
     magerun2 dev:template-hints-blocks --off
     magerun2 dev:template-hints --off
 }
 
+_DUCTN_M2+=(install)
+_dev:m2:install() {
+    curl -O https://files.magerun.net/n98-magerun2.phar && chmod +x n98-magerun2.phar && sudo mv n98-magerun2.phar /usr/local/bin/magerun2
+}
+
 --m2() {
-    "_dev:$*"
+    "_dev:m2:$*"
 }
 
 --m2:completion:commands() {
