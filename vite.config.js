cf4c2a1bd32b4629e194bf3326fb855327d83758
Tran Ngoc Duc <caothu91@gmail.com>
2023-04-01 20:39:07 +0700

    - build js and css use `laravel-vite-plugin`   - use bootstrap css and js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import path from "path";
+import fs from "fs";
+
+const host = "admin.diepxuan.com";
+const port = "443";
 
 export default defineConfig({
     plugins: [
@@ -9,6 +13,21 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        https: {
+            key: fs.readFileSync("./.cert/key.pem"),
+            cert: fs.readFileSync("./.cert/cert.pem"),
+        },
+        proxy: {
+            "^(?!(/@vite|/resources|/node_modules))": {
+                target: `https://${host}:${port}`,
+                changeOrigin: true,
+            },
+        },
+        host: "0.0.0.0",
+        port: 5173,
+        hmr: { host },
+    },
     resolve: {
         alias: {
             "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
