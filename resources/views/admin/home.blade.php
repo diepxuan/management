8d43a7630f50c05b374ec04869de7f9c4b89905a
Tran Ngoc Duc <caothu91@gmail.com>
2023-04-02 13:16:38 +0700

    - list VM name and hosts - Frontend   - top narbar   - left narbar   - vm client layout

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,4 +1,21 @@
 @extends('admin.layout')
-@section('content')
-    {{ $data['name'] }}
+@section('main')
+    <div class="row">
+        <div class="col">
+        </div>
+        <div class="col">
+            <h3>VMs</h3>
+            <div class="accordion accordion-flush" id="accordionVms">
+
+                @foreach ($vms as $vm)
+                    @include('sys/vm/item', $vm)
+                @endforeach
+
+            </div>
+
+        </div>
+        <div class="col">
+            3 of 3
+        </div>
+    </div>
 @endsection
