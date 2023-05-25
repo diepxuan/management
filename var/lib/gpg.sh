c40e2b00b09d8dc403a8a73d95bcf75b67fe1339
Tran Ngoc Duc <caothu91@gmail.com>
2023-05-25 20:19:42 +0700

    3.1.9
      * OpenPGP and GNU Privacy Guard
        - backup
        - import

---
+++ var/lib/gpg.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#!/bin/bash
+
+_DUCTN_COMMANDS+=("gpg")
+--gpg() {
+    gpg --list-secret-keys --keyid-format LONG
+}
+
+_DUCTN_COMMANDS+=("gpg:export")
+--gpg:export() {
+    user=$(whoami)
+    pwd=$(eval echo "~$user")
+    [[ -n "$*" ]] && pwd=$1
+
+    gpg --export --export-options backup --output "$pwd/public.gpg"
+    gpg --export-secret-keys --export-options backup --output "$pwd/private.gpg"
+    gpg --export-ownertrust >"$pwd/trust.gpg"
+}
+
+_DUCTN_COMMANDS+=("gpg:import")
+--gpg:import() {
+    gpg --import public.gpg
+    gpg --import private.gpg
+    gpg --import-ownertrust trust.gpg
+}
