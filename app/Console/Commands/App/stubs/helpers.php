68c193771c59ac9cd5ab0e82e9f02fe1f5f8c9f9
Tran Ngoc Duc <caothu91@gmail.com>
2023-09-14 07:52:06 +0700

    fix realpath function in phar

--- app/Console/Commands/App/stubs/helpers.php
+++ app/Console/Commands/App/stubs/helpers.php
@@ -11,66 +11,68 @@
 //     return $arg;
 // }, ZEND_ACC_STATIC);
 
-if (function_exists("runkit7_function_rename"))
-    runkit7_function_rename('realpath', 'org_realpath');
-
-if (function_exists("runkit7_function_add"))
-    runkit7_function_add('realpath', '$path', 'return phar_realpath($path);');
-
+if (!function_exists('absolutepath')) {
+    function phar_absolutepath($path)
+    {
+        $path = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, $path);
+        $parts = array_filter(explode(DIRECTORY_SEPARATOR, $path), 'strlen');
+        $absolutes = array();
+        foreach ($parts as $part) {
+            if ('.' == $part) continue;
+            if ('..' == $part) {
+                array_pop($absolutes);
+            } else {
+                $absolutes[] = $part;
+            }
+        }
+        return implode(DIRECTORY_SEPARATOR, $absolutes);
+    }
+}
 
 function phar_realpath($path)
 {
-    $realpath = org_realpath($path);
-    if ($realpath == false && file_exists($path)) {
-        if (str_starts_with($path, 'phar://'))
-            $realpath = $path;
-        elseif (strpos($path, 'phar://') === 0)
-            $realpath = $path;
-
-        $realpath = trim($path, DIRECTORY_SEPARATOR);
-        // if (is_dir($path)) {
-        //     $realpath = trim($path, DIRECTORY_SEPARATOR);
-        // }
+    $realPath = org_realpath($path);
+    if ($realPath == false && file_exists($path)) {
+        if (str_starts_with($path, 'phar:///'))
+            $strStart = 'phar:///';
+        $realPath = str_replace($strStart, '', $path);
+        $realPath = phar_absolutepath($realPath);
+        $realPath = "$strStart$realPath";
     }
-    return $realpath;
+    return $realPath;
 }
 
-if (function_exists("runkit7_method_redefine"))
-    runkit7_method_redefine(
-        $class_name = \Illuminate\Log\LogManager::class,
-        $method_name = 'createEmergencyLogger',
-        function () {
-            $config = $this->configurationFor('emergency');
+runkit7_function_rename('realpath', 'org_realpath');
 
-            $logPath = $config['path'] ?? $this->app->storagePath() . '/logs/laravel.log';
-            if (str_starts_with($logPath, 'phar://') || strpos($logPath, 'phar://') === 0)
-                $logPath = '/var/log/ductn.log';
+runkit7_function_add('realpath', '$path', 'return phar_realpath($path);');
 
-            $handler = new \Monolog\Handler\StreamHandler(
-                $logPath,
-                $this->level(['level' => 'debug'])
-            );
+runkit7_method_add(
+    $class_name = \Symfony\Component\Finder\SplFileInfo::class,
+    $method_name = 'getRealPath',
+    function () {
+        return parent::getRealPath() ?: phar_realpath(parent::getPathname());
+    }
+);
 
-            return new \Illuminate\Log\Logger(
-                new \Monolog\Logger('laravel', $this->prepareHandlers([$handler])),
-                $this->app['events']
-            );
-        }
-    );
 
-if (function_exists("runkit7_method_add"))
-    runkit7_method_add(
-        $class_name = \Symfony\Component\Finder\SplFileInfo::class,
-        $method_name = 'getRealPath',
-        function () {
-            $realPath = parent::getRealPath();
-            $path = $this->getPathname();
-            if ($realPath == false && file_exists($path)) {
-                if (str_starts_with($path, 'phar://'))
-                    $realPath = $path;
-                if (strpos($path, 'phar://') === 0)
-                    $realPath = $path;
-            }
-            return $realPath;
-        }
-    );
+runkit7_method_redefine(
+    $class_name = \Illuminate\Log\LogManager::class,
+    $method_name = 'createEmergencyLogger',
+    function () {
+        $config = $this->configurationFor('emergency');
+
+        $logPath = $config['path'] ?? $this->app->storagePath() . '/logs/laravel.log';
+        if (str_starts_with($logPath, 'phar://') || strpos($logPath, 'phar://') === 0)
+            $logPath = '/var/log/ductn.log';
+
+        $handler = new \Monolog\Handler\StreamHandler(
+            $logPath,
+            $this->level(['level' => 'debug'])
+        );
+
+        return new \Illuminate\Log\Logger(
+            new \Monolog\Logger('laravel', $this->prepareHandlers([$handler])),
+            $this->app['events']
+        );
+    }
+);
