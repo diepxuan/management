85195f8e06d2af2cd627bbeff7510d121ccd57ab
Tran Ngoc Duc <caothu91@gmail.com>
2023-09-17 14:45:22 +0700

    fix autoload helpers

--- app/Console/Commands/App/stubs/helpers.php
+++ app/Console/Commands/App/stubs/helpers.php
@@ -29,50 +29,55 @@ if (!function_exists('absolutepath')) {
     }
 }
 
-function phar_realpath($path)
-{
-    $realPath = org_realpath($path);
-    if ($realPath == false && file_exists($path)) {
-        if (str_starts_with($path, 'phar:///'))
-            $strStart = 'phar:///';
-        $realPath = str_replace($strStart, '', $path);
-        $realPath = phar_absolutepath($realPath);
-        $realPath = "$strStart$realPath";
+if (!function_exists('phar_realpath')) {
+    function phar_realpath($path)
+    {
+        $realPath = org_realpath($path);
+        if ($realPath == false && file_exists($path)) {
+            if (str_starts_with($path, 'phar:///'))
+                $strStart = 'phar:///';
+            $realPath = str_replace($strStart, '', $path);
+            $realPath = phar_absolutepath($realPath);
+            $realPath = "$strStart$realPath";
+        }
+        return $realPath;
     }
-    return $realPath;
 }
 
-runkit7_function_rename('realpath', 'org_realpath');
+try {
+    runkit7_function_rename('realpath', 'org_realpath');
 
-runkit7_function_add('realpath', '$path', 'return phar_realpath($path);');
+    runkit7_function_add('realpath', '$path', 'return phar_realpath($path);');
 
-runkit7_method_add(
-    $class_name = \Symfony\Component\Finder\SplFileInfo::class,
-    $method_name = 'getRealPath',
-    function () {
-        return parent::getRealPath() ?: phar_realpath(parent::getPathname());
-    }
-);
+    runkit7_method_add(
+        $class_name = \Symfony\Component\Finder\SplFileInfo::class,
+        $method_name = 'getRealPath',
+        function () {
+            return parent::getRealPath() ?: phar_realpath(parent::getPathname());
+        }
+    );
 
+    runkit7_method_redefine(
+        $class_name = \Illuminate\Log\LogManager::class,
+        $method_name = 'createEmergencyLogger',
+        function () {
+            $config = $this->configurationFor('emergency');
 
-runkit7_method_redefine(
-    $class_name = \Illuminate\Log\LogManager::class,
-    $method_name = 'createEmergencyLogger',
-    function () {
-        $config = $this->configurationFor('emergency');
+            $logPath = $config['path'] ?? $this->app->storagePath() . '/logs/laravel.log';
+            if (str_starts_with($logPath, 'phar://') || strpos($logPath, 'phar://') === 0)
+                $logPath = '/var/log/ductn.log';
 
-        $logPath = $config['path'] ?? $this->app->storagePath() . '/logs/laravel.log';
-        if (str_starts_with($logPath, 'phar://') || strpos($logPath, 'phar://') === 0)
-            $logPath = '/var/log/ductn.log';
+            $handler = new \Monolog\Handler\StreamHandler(
+                $logPath,
+                $this->level(['level' => 'debug'])
+            );
 
-        $handler = new \Monolog\Handler\StreamHandler(
-            $logPath,
-            $this->level(['level' => 'debug'])
-        );
-
-        return new \Illuminate\Log\Logger(
-            new \Monolog\Logger('laravel', $this->prepareHandlers([$handler])),
-            $this->app['events']
-        );
-    }
-);
+            return new \Illuminate\Log\Logger(
+                new \Monolog\Logger('laravel', $this->prepareHandlers([$handler])),
+                $this->app['events']
+            );
+        }
+    );
+} catch (\Throwable $th) {
+    //
+}
