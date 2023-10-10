b027bf9d9b3e159633fe9cc94d16530fb1784a95
Tran Ngoc Duc <caothu91@gmail.com>
2023-10-10 16:48:12 +0700

    ISUES: error: version number does not start with digit

--- ci/update_packages.sh
+++ ci/update_packages.sh
@@ -5,10 +5,6 @@ set -e
 # set -u
 . $(dirname $(realpath "$BASH_SOURCE"))/head.sh
 
-# Update module runkit7 release latest
-old_release_tag=$(cat $changelog | head -n 1 | awk '{print $2}' | cut -d '+' -f1 | sed 's|[()]||g')
-sed -i -e "0,/$old_release_tag/ s/$old_release_tag/$release_tag/g" $changelog
-
 # Update os release latest
 old_release_os=$(cat $changelog | head -n 1 | awk '{print $2}' | cut -d '+' -f2 | cut -d '~' -f1)
 sed -i -e "0,/$old_release_os/ s/$old_release_os/${DISTRIB}${RELEASE}/g" $changelog
