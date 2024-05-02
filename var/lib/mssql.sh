2a001a636e2daa4f4e70359d79102e36fff2f791
Tran Ngoc Duc <caothu91@gmail.com>
2024-05-02 20:13:11 +0700

    mssql support

--- var/lib/mssql.sh
+++ var/lib/mssql.sh
@@ -1,15 +1,59 @@
 #!/usr/bin/env bash
 #!/bin/bash
 
---sqlsrv:apt:install() {
-    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-    # sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/16.04/mssql-server-2019.list)"
+--sqlsrv:apt() {
+    if ! [[ "16.04 18.04 20.04 22.04" == *"$(lsb_release -rs)"* ]]; then
+        echo "Ubuntu $(lsb_release -rs) is not currently supported."
+        exit
+    fi
+
+    [[ -f /etc/os-release ]] && . /etc/os-release
+    [[ -f /etc/lsb-release ]] && . /etc/lsb-release
+
+    RELEASE=${RELEASE:-$VERSION_ID}
+    RELEASE=${RELEASE:-$UBUNTU_CODENAME}
+
+    DISTRIB=${DISTRIB:-$DISTRIB_ID}
+    DISTRIB=${DISTRIB:-$ID}
+    DISTRIB=$(echo "$DISTRIB" | awk '{print tolower($0)}')
+
+    # wget -qO- https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | tee /etc/apt/trusted.gpg.d/microsoft-prod.asc
+    # curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
     # sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/18.04/mssql-server-2019.list)"
-    sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/20.04/mssql-server-2019.list)"
+    # sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/20.04/mssql-server-2019.list)"
     # sudo apt update
+
+    command -v add-apt-repository >/dev/null 2>&1 &&
+        add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/$(DISTRIB)/$(RELEASE)/prod.list)" ||
+        wget -qO- https://packages.microsoft.com/config/$(DISTRIB)/$(RELEASE)/prod.list | tee /etc/apt/sources.list.d/mssql-release.list
+
 }
 
---sqlsrv:ad:spn() {
+--sqlsrv:php() {
+    apt update
+    ACCEPT_EULA=Y apt install -y msodbcsql17
+
+    apt install -y php-dev php-pear
+
+    PHP_PATH=$(php --ini | grep "Configuration File (php.ini)" | sed -e "s|.*:\s*||" -e "s/\/cli$//")
+
+    printf "priority=20\nextension=sqlsrv.so\n" |
+        tee $PHP_PATH/mods-available/sqlsrv.ini
+
+    printf "priority=30\nextension=pdo_sqlsrv.so\n" |
+        tee $PHP_PATH/mods-available/pdo_sqlsrv.ini
+
+    phpenmod sqlsrv pdo_sqlsrv
+    service apache2 start
+}
+
+--sqlsrv:php_disable() {
+    phpdismod sqlsrv pdo_sqlsrv
+    service apache2 restart
+}
+
+__sqlsrv:ad:spn() {
     # From DC
 
     # Create User
@@ -66,8 +110,7 @@
 #sudo apt install -y unixodbc tdsodbc php?.?-sybase &>/dev/null
 #sudo phpenmod sybase
 
-_DUCTN_COMMANDS+=("sqlsrv:php:install")
---sqlsrv:php:install() {
+__sqlsrv:php:install() {
     # Install PHP and other required packages
     #########################################
     ductn php:apt:install
@@ -92,16 +135,15 @@ _DUCTN_COMMANDS+=("sqlsrv:php:install")
 
     # Install the PHP Driver for SQL Server
     #######################################
-    # sudo apt install php-pear gcc g++ make autoconf libc-dev pkg-config libxml2-dev -y
-    sudo apt install php-pear -y
-    sudo pecl channel-update pecl.php.net
+    # sudo apt install php-pear -y
+    # sudo pecl channel-update pecl.php.net
     sudo pecl install sqlsrv pdo_sqlsrv
-    # sudo pecl install sqlsrv-5.7.0preview pdo_sqlsrv-5.7.0preview
+
 }
 
 _DUCTN_COMMANDS+=("sqlsrv:php:enable")
---sqlsrv:php:enable() {
-    --sqlsrv:php:disable
+__sqlsrv:php:enable() {
+    __sqlsrv:php:disable
     # printf "priority=20\nextension=sqlsrv.so\n" | sudo tee /etc/php/5.6/mods-available/sqlsrv.ini
     # printf "priority=30\nextension=pdo_sqlsrv.so\n" | sudo tee /etc/php/5.6/mods-available/pdo_sqlsrv.ini
 
@@ -122,7 +164,7 @@ _DUCTN_COMMANDS+=("sqlsrv:php:enable")
 
     # printf "; priority=20\nextension=sqlsrv.so\n" | sudo tee /etc/php/8.0/mods-available/sqlsrv.ini
     # printf "; priority=30\nextension=pdo_sqlsrv.so\n" | sudo tee /etc/php/8.0/mods-available/pdo_sqlsrv.ini
-    $(php --ini | grep "Configuration File (php.ini)" | sed -e "s|.*:\s*||" -e "s/\/cli$//")
+    # $(php --ini | grep "Configuration File (php.ini)" | sed -e "s|.*:\s*||" -e "s/\/cli$//")
     printf "priority=20\nextension=sqlsrv.so\n" | sudo tee $(php --ini | grep "Configuration File (php.ini)" | sed -e "s|.*:\s*||" -e "s/\/cli$//")/mods-available/sqlsrv.ini
     printf "priority=30\nextension=pdo_sqlsrv.so\n" | sudo tee $(php --ini | grep "Configuration File (php.ini)" | sed -e "s|.*:\s*||" -e "s/\/cli$//")/mods-available/pdo_sqlsrv.ini
 
@@ -134,30 +176,30 @@ _DUCTN_COMMANDS+=("sqlsrv:php:enable")
 }
 
 _DUCTN_COMMANDS+=("sqlsrv:php:disable")
---sqlsrv:php:disable() {
+__sqlsrv:php:disable() {
     sudo phpdismod sqlsrv pdo_sqlsrv
     sudo /usr/sbin/service apache2 restart
 }
 
 _DUCTN_COMMANDS+=("mssql:php:install")
 --mssql:php:install() {
-    --sqlsrv:php:install
+    __sqlsrv:php:install
 }
 
 _DUCTN_COMMANDS+=("mssql:php:enable")
 --mssql:php:enable() {
-    --sqlsrv:php:enable
+    __sqlsrv:php:enable
     sudo /usr/sbin/service apache2 start
 }
 
 _DUCTN_COMMANDS+=("mssql:php:disable")
 --mssql:php:disable() {
-    --sqlsrv:php:disable
+    __sqlsrv:php:disable
 }
 #!/usr/bin/env bash
 #!/bin/bash
 
---sqlsrv:apt:install() {
+__sqlsrv:apt:install() {
     wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
     # sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/16.04/mssql-server-2019.list)"
     # sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/18.04/mssql-server-2019.list)"
@@ -166,8 +208,8 @@ _DUCTN_COMMANDS+=("mssql:php:disable")
 }
 
 _DUCTN_COMMANDS+=("sqlsrv:install")
---sqlsrv:install() {
-    --sqlsrv:apt:install
+__sqlsrv:install() {
+    __sqlsrv:apt:install
 
     # Install SQL Server
     ####################
@@ -180,9 +222,9 @@ _DUCTN_COMMANDS+=("sqlsrv:install")
 
 _DUCTN_COMMANDS+=("mssql:install")
 --mssql:install() {
-    --sqlsrv:install
+    __sqlsrv:install
 }
 
---sqlsrv:cron() {
-    --sqlsrv:install
+__sqlsrv:cron() {
+    __sqlsrv:install
 }
