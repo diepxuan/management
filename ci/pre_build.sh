34491b81b655332316bc358e71aed3d2d1ec33d2
Tran Ngoc Duc <caothu91@gmail.com>
2024-06-30 23:53:57 +0700

    download phpactor.phar library

--- ci/pre_build.sh
+++ ci/pre_build.sh
@@ -10,3 +10,6 @@ cat $source_lib/git.sh >>$source_dir/ductn
 
 cat $source_var/ductn >>$source_dir/ductn
 cat $source_var/m2 >$source_dir/m2
+
+mkdir -p $source_dir/lib/
+curl -Lo $source_dir/lib/phpactor.phar https://github.com/phpactor/phpactor/releases/latest/download/phpactor.phar
