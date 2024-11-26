d09a4b1115437504e47cdf4810109a7628616c79
Trần Ngọc Đức <caothu91@gmail.com>
2024-11-26 19:44:26 +0700

    Update swap.sh

--- var/lib/swap.sh
+++ var/lib/swap.sh
@@ -12,6 +12,7 @@ _DUCTN_COMMANDS+=("swap:install")
     --swap:remove
     sudo rm -rf /swapfile
     sudo fallocate -l 2G /swapfile
+    #sudo dd if=/dev/zero of=/swapfile bs=1M count=2048
     sudo chmod 600 /swapfile
     sudo mkswap /swapfile
     sudo swapon /swapfile
