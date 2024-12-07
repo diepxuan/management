10ea61e3806f4eab1820037f493cb6548d9c5995
Tran Ngoc Duc <caothu91@gmail.com>
2024-12-07 13:22:02 +0700

    create lar command for laravel

--- ci/pre_build.sh
+++ ci/pre_build.sh
@@ -6,10 +6,12 @@ set -e
 . $(dirname $(realpath "$BASH_SOURCE"))/head.sh
 
 cat $source_lib/php.m2.sh >>$source_dir/ductn
+cat $source_lib/php.lar.sh >>$source_dir/ductn
 cat $source_lib/git.sh >>$source_dir/ductn
 cat $source_lib/log.sh >>$source_dir/ductn
 
 cat $source_var/ductn >>$source_dir/ductn
 cat $source_var/m2 >$source_dir/m2
+cat $source_var/lar >$source_dir/lar
 
 mkdir -p $source_dir/lib/
