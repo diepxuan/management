a36072bd82cfbdb5e3621600bcda15d15b9a1af1
Tran Ngoc Duc <caothu91@gmail.com>
2024-12-31 11:39:24 +0700

    Update build workflow and package script to enable conditional execution and streamline source management

--- ci/put_packages.sh
+++ ci/put_packages.sh
@@ -7,9 +7,22 @@ set -e
 
 start_group "put package to DiepXuan PPA"
 git clone --depth 1 --branch main git@github.com:diepxuan/ppa.git
-cp -r $dists_dir/*.deb $ppa_dir/dists/
+
+rm -rf $ppa_dir/src/$repository
+mkdir -p $ppa_dir/src/$repository/
+cp -r $source_dir/. $ppa_dir/src/$repository/
 cd $ppa_dir
+
+if [ "$(git status src/ --porcelain=v1 2>/dev/null | wc -l)" != "0" ]; then
+    git add src/
+    git commit -m "Update version at $(date +'%d-%m-%y')"
+    git fetch -ap
+    git pull --rebase -X ours
+    git push origin HEAD:main
+fi
+
 git add dists/
 git commit -m "Add .deb package"
 git push origin main
+
 end_group
