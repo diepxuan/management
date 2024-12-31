6b9be6d4ddb8df25145de138337b79d886c6acf7
Tran Ngoc Duc <caothu91@gmail.com>
2024-12-31 11:21:52 +0700

    Add SSH configuration to build workflow and streamline package deployment

--- ci/put_packages.sh
+++ ci/put_packages.sh
@@ -5,14 +5,7 @@ set -e
 # set -u
 . $(dirname $(realpath "$BASH_SOURCE"))/head.sh
 
-mkdir ~/.ssh/
-chmod 700 ~/.ssh
-echo "$SSH_ID_RSA" >~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-ssh-keygen -f ~/.ssh/id_rsa -y >~/.ssh/id_rsa.pub
-
 start_group "put package to DiepXuan PPA"
-
 git clone --depth 1 --branch main git@github.com:diepxuan/ppa.git
 cp -r $dists_dir/*.deb $ppa_dir/dists/
 cd $ppa_dir
